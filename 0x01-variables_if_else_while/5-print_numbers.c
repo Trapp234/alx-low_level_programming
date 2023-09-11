f80cd1a2d40b1fa2697a01a431938e30217e4e21
Trapp234 <kennedyboamah10@gmail.com>
2023-09-11 13:13:41 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 -9
+ *
+ * Return: (0) Always
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+			i++;
+	}
+		printf("\n");
+		return (0);
+}
